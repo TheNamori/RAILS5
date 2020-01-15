106014b3d620068746074d8f275c9813b0f9d83f
matheus.lamboia <matheus.lamboia@totalip.com.br>
2020-01-15 16:16:19 -0300

    topzera

--- RAILS/app/controllers/contacts_controller.rb
+++ RAILS/app/controllers/contacts_controller.rb
@@ -10,6 +10,8 @@ class ContactsController < ApplicationController
   # GET /contacts/1
   # GET /contacts/1.json
   def show
+    @contact = Contact.find(params[:id])
+    @kind_selection = Kind.all
   end
 
   # GET /contacts/new
